24d5b63f69aaec5e6d91920f628938c99e8be091
Seth Lytton <slytton@gmail.com>
2016-06-10 14:50:59 -0600

    Refactor to user standard Number arrays instead of lists

--- src/main/java/com/galvanize/grading/Grades.java
+++ src/main/java/com/galvanize/grading/Grades.java
@@ -1,6 +1,5 @@
-package com.sethlytton.grading;
+package com.galvanize.grading;
 
-import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import org.json.simple.JSONObject;
 
@@ -12,26 +11,28 @@ import java.util.List;
  * Created by gschool on 6/8/16.
  */
 
-public class Grading {
+public class Grades {
 
-    final static Logger logger = Logger.getLogger(Grading.class);
+    final static Logger logger = Logger.getLogger(Grades.class);
+    private Number[] grades;
 
-    public static void main(String[] args) {
-        BasicConfigurator.configure();
+    public Grades(Number[] grades){
+        this.grades = grades;
     }
 
-    public String[] differences(List<Number> grades) {
+    public String[] differences() {
+        Number[] grades = this.grades;
 
-        if (grades.size() <= 1) return new String[0];
+        if (grades.length <= 1) return new String[0];
 
-        String[] results = new String[grades.size() - 1];
+        String[] results = new String[grades.length - 1];
 
-        Integer previousGrade = grades.get(0).intValue();
+        int previousGrade = grades[0].intValue();
 
         String result;
 
-        for (int i = 1; i < grades.size(); i++) {
-            Integer grade = grades.get(i).intValue();
+        for (int i = 1; i < grades.length; i++) {
+            int grade = grades[i].intValue();
             result = "";
             if (previousGrade < grade) result = "up";
             if (previousGrade == grade) result = "even";
@@ -42,14 +43,15 @@ public class Grading {
         return results;
     }
 
-    public boolean inDecline(List <Number> grades){
+    public boolean inDecline(){
+        Number[] grades = this.grades;
         int declineCount = 0;
-        if(grades.size() <= 1) return false;
+        if(grades.length <= 1) return false;
 
-        int previousGrade = grades.get(0).intValue();
+        int previousGrade = grades[0].intValue();
 
-        for (int i = 1; i < grades.size(); i++){
-            int grade = grades.get(i).intValue();
+        for (int i = 1; i < grades.length; i++){
+            int grade = grades[i].intValue();
             if(previousGrade < grade) declineCount = 0;
             if(previousGrade > grade) declineCount++;
             previousGrade = grade;
@@ -58,12 +60,12 @@ public class Grading {
     }
 
     public int[] numInDecline(JSONObject data) {
-
         Iterator<List<Number>> iterator  = data.values().iterator();
         int[] results = {0,0};
 
         while(iterator.hasNext()) {
-            Boolean inDecline = new Grading().inDecline(iterator.next());
+            Number[] grades = iterator.next().toArray(new Number[0]);
+            Boolean inDecline = new Grades(grades).inDecline();
             if (inDecline) {
                 results[0]++;
             } else {
--- src/test/java/com/galvanize/grading/GradesTest.groovy
+++ src/test/java/com/galvanize/grading/GradesTest.groovy
@@ -1,4 +1,4 @@
-package com.sethlytton.grading
+package com.galvanize.grading
 
 import org.json.simple.JSONObject
 import org.json.simple.parser.JSONParser
@@ -9,45 +9,45 @@ import java.nio.file.Paths
 /**
  * Created by gschool on 6/9/16.
  */
-class GradingTest extends groovy.util.GroovyTestCase {
+class GradesTest extends groovy.util.GroovyTestCase {
 
     void testDifferences() {
-        println new Grading().differences([6, 3, 5, 4, 3, 4, 4, 5])
+        println new Grades([6, 3, 5, 4, 3, 4, 4, 5] as Number[]).differences()
         assertTrue "differences should return an array with up, down and even.",
-                Arrays.equals( new Grading().differences([6, 3, 5, 4, 3, 4, 4, 5]), ["down", "up", "down", "down", "up", "even", "up"] as String[])
+                Arrays.equals( new Grades([6, 3, 5, 4, 3, 4, 4, 5] as Number[]).differences(), ["down", "up", "down", "down", "up", "even", "up"] as String[])
         assertTrue "differences returns an empty array when given an empty array",
-                Arrays.equals(new Grading().differences([]), [] as String[]);
+                Arrays.equals(new Grades([] as Number[]).differences(), [] as String[]);
         assertTrue "differences returns an empty array when given an array with 1 element",
-                Arrays.equals(new Grading().differences([8]), [] as String[]);
+                Arrays.equals(new Grades([8] as Number[]).differences(), [] as String[]);
     }
 
     void testInDecline(){
         assertFalse "returns false for empty array: " + [],
-                new Grading().inDecline([])
+                new Grades([] as Number[]).inDecline()
         assertFalse "returns false for an array with one element: " + [8],
-                new Grading().inDecline([8])
+                new Grades([8] as Number[]).inDecline()
 
         assertTrue "returns true if the last three grades are downs: " + [5,4,3,2],
-                new Grading().inDecline([5,4,3,2])
+                new Grades([5,4,3,2] as Number[]).inDecline()
         assertTrue "returns true if three downs separated by evens: " + [5,5,4,4,3,3,2,2],
-                new Grading().inDecline([5,5,4,4,3,3,2,2])
+                new Grades([5,5,4,4,3,3,2,2] as Number[]).inDecline()
         assertTrue "returns true if three downs separated by evens: " + [5,4,4,3,2,2],
-                new Grading().inDecline([5,4,4,3,2,2])
+                new Grades([5,4,4,3,2,2] as Number[]).inDecline()
         assertTrue "returns true if three downs separated by evens: " + [5,5,4,3,3,3,3,2,2,1],
-                new Grading().inDecline([5,5,4,3,3,3,3,2,2,1])
+                new Grades([5,5,4,3,3,3,3,2,2,1] as Number[]).inDecline()
         assertTrue "returns true if three downs separated by evens: " + [10,10,10,9,9,8,8,8,8,7],
-                new Grading().inDecline([10,10,10,9,9,8,8,8,8,7])
+                new Grades([10,10,10,9,9,8,8,8,8,7] as Number[]).inDecline()
         assertTrue "returns true if three downs separated by evens: " + [76,62,7,7,1],
-                new Grading().inDecline([76,62,7,7,1])
+                new Grades([76,62,7,7,1] as Number[]).inDecline()
 
         assertFalse "returns false if there are fewer than 3 downs: " + [5,4,3],
-                new Grading().inDecline([5,4,3])
+                new Grades([5,4,3] as Number[]).inDecline()
         assertFalse "returns false if there are fewer than 3 downs: " + [5,5,4,4,3,3],
-                new Grading().inDecline([5,5,4,4,3,3])
+                new Grades([5,5,4,4,3,3] as Number[]).inDecline()
         assertFalse "returns false if there are fewer than 3 downs: " + [5,5,4,4,5,3,2],
-                new Grading().inDecline([5,5,4,4,5,3,2])
+                new Grades([5,5,4,4,5,3,2] as Number[]).inDecline()
         assertFalse "returns false if there are fewer than 3 downs: " + [71,4,27,21,69,62,65,64,38,35,41,19],
-                new Grading().inDecline([71,4,27,21,69,62,65,64,38,35,41,19])
+                new Grades([71,4,27,21,69,62,65,64,38,35,41,19] as Number[]).inDecline()
     }
 
     void testNumInDecline(){
@@ -57,10 +57,8 @@ class GradingTest extends groovy.util.GroovyTestCase {
         try {
             Object obj = parser.parse(file);
             obj = (JSONObject) obj
-            println "hello"
-            println new Grading().numInDecline((obj))
             assertTrue "returns an array with the number of people in decline and the number of people not in decline",
-                    Arrays.equals( new Grading().numInDecline((obj)), [27, 73] as int[])
+                    Arrays.equals( new Grades().numInDecline((obj)), [27, 73] as int[])
         }catch (Exception e) {
             e.printStackTrace();
         }

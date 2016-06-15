12df7db9a06ae0e37488c6b414bcd8da97554108
Seth Lytton <slytton@gmail.com>
2016-06-15 10:31:18 -0600

    Complete recursive iteration exercises through some.

---
+++ src/main/java/com/galvanize/recursiveIteration/RecursiveIteration.java
@@ -0,0 +1,117 @@
+package com.galvanize.recursiveIteration;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * Created by gschool on 6/14/16.
+ */
+
+public class RecursiveIteration {
+    private int[] arr;
+    public RecursiveIteration(int[] arr){
+        this.arr = arr;
+    }
+
+//    **** #max() ****
+    public Integer max(){
+        if(this.arr.length == 0) return null;
+        return max(0);
+    }
+
+    public Integer max(Integer index){
+        if(index < this.arr.length - 1){
+            int value = this.arr[index];
+            int max = max(++index);
+            return ( max >= value ) ? max : value;
+        }else{
+            return this.arr[index];
+        }
+    }
+
+//    **** #min() ****
+    public Integer min(){
+        if(this.arr.length == 0) return null;
+        return min(0);
+    }
+
+    public Integer min(Integer index) {
+        if (index < this.arr.length - 1) {
+            int value = this.arr[index];
+            int min = min(++index);
+            return (min < value) ? min : value;
+        }else{
+            return this.arr[index];
+        }
+    }
+
+//    **** #filter() ****
+    public List<Integer> filter(Predicate<Integer> fn){
+        List<Integer> filtered = new ArrayList<Integer>();
+        return filter(0, filtered, fn);
+    }
+
+    public List<Integer> filter(Integer index, List<Integer> filtered, Predicate<Integer> fn) {
+        if (index < this.arr.length) {
+            if(fn.test(this.arr[index])){
+                filtered.add(this.arr[index]);
+            }
+            return filter(++index, filtered, fn);
+        }else{
+            return filtered;
+        }
+    }
+
+//    **** #reject() ****
+    public List<Integer> reject(Predicate<Integer> fn){
+        return reject(0, new ArrayList<Integer>(), fn);
+    }
+
+    public List<Integer> reject(Integer index, List<Integer> rejected, Predicate<Integer> fn){
+        if(index < this.arr.length) {
+            int val = this.arr[index];
+            if(!fn.test(val)) {
+                rejected.add(val);
+            }
+            return reject(++index, rejected, fn);
+        }else{
+            return rejected;
+        }
+    }
+
+
+//    **** #every() ****
+    public Boolean every(Predicate<Integer> fn){
+        if(this.arr.length == 0) return true;
+        return every(0, fn);
+    }
+
+    public Boolean every(int index, Predicate<Integer> fn){
+        if(index < this.arr.length){
+            if(!fn.test(this.arr[index])) return false;
+            return every(++index, fn);
+        }else{
+            return true;
+        }
+    }
+
+
+//    **** #some() ****
+    public Boolean some(Predicate<Integer> fn) {
+        if(this.arr.length == 0) return false;
+        return some(0, fn);
+    }
+
+    public Boolean some(int index, Predicate<Integer> fn) {
+        if(index < this.arr.length){
+            if(fn.test(this.arr[index])) return true;
+            return some(++index, fn);
+        }else{
+            return false;
+        }
+    }
+}
+
+
+
---
+++ src/test/java/com/galvanize/recursiveIteration/RecursiveIterationTest.groovy
@@ -0,0 +1,207 @@
+package com.galvanize.recursiveIteration
+
+import spock.lang.Specification
+
+import java.util.function.Predicate
+
+/**
+ * Created by gschool on 6/14/16.
+ */
+class recursiveIterationTest extends Specification {
+    def "#max returns the maximum value of the array"(Integer[] arr, Integer maxValue) {
+        expect:
+        new RecursiveIteration(arr).max() == maxValue;
+
+        where:
+        arr               ||  maxValue
+        [1]               ||  1
+        [1,2,3]           ||  3
+        [20,10,30]        ||  30
+        [5,10,15,2,-1,4]  ||  15
+        []                ||  null
+    }
+
+    def "#min returns the minimum value of the array"(Integer[] arr, Integer minValue) {
+        expect:
+        new RecursiveIteration(arr).min() == minValue;
+
+        where:
+        arr               ||  minValue
+        [1]               ||  1
+        [1,2,3]           ||  1
+        [20,10,30]        ||  10
+        [5,10,15,2,-1,4]  ||  -1
+        []                ||  null
+    }
+
+    def "#filter returns an array that contains only matching items"(int[] input, List<Integer> result) {
+        def fn = { x -> x >= 10 } as Predicate<Integer>;
+
+        expect:
+        new RecursiveIteration(input).filter(fn) == result
+
+        where:
+        input         ||  result
+        [1,2,3]       ||  []
+        [10, 20, 30]  ||  [10, 20, 30]
+        [5, 10, 15]   ||  [10, 15]
+        []            ||  []
+    }
+
+    def "#reject returns an array conRtaining only the non-matching items"(int[] input, List<Integer> result) {
+        def fn = { x -> x >= 10 } as Predicate<Integer>;
+
+        expect:
+        new RecursiveIteration(input).reject(fn) == result
+
+        where:
+        input         ||  result
+        [1,2,3]       ||  [1,2,3]
+        [10, 20, 30]  ||  []
+        [5, 10, 15]   ||  [5]
+        []            ||  []
+    }
+
+    def "#every returns true if every item in the array matches" (int[] input, Boolean result) {
+        def fn = { x -> x >= 10 } as Predicate<Integer>
+
+        expect:
+        new RecursiveIteration(input).every(fn) == result;
+
+        where:
+        input         ||  result
+        [1,2,3]       ||  false
+        [10, 20, 30]  ||  true
+        [5, 10, 15]   ||  false
+        []            ||  true
+    }
+
+    def "#some returns true if at least one item in the array matches" (int[] input, Boolean result) {
+        def fn = { x -> x >= 10 } as Predicate<Integer>
+
+        expect:
+        new RecursiveIteration(input).some(fn) == result;
+
+        where:
+        input         ||  result
+        [1,2,3]       ||  false
+        [10, 20, 30]  ||  true
+        [5, 10, 15]   ||  true
+        []            ||  false
+    }
+}
+
+
+//    describe("some", () => {
+//        it("returns true if at least one item in the array matches", () => {
+//            const fn = (val) => val >= 10;
+//
+//            expect(lib.some([1,2,3], fn)).to.deep.equal(false)
+//            expect(lib.some([10,20,30], fn)).to.deep.equal(true)
+//            expect(lib.some([5, 10, 15], fn)).to.deep.equal(true)
+//            expect(lib.some([], fn)).to.deep.equal(false)
+//        })
+//
+//        it("does not use loops", checkForLoops('some'))
+//    })
+//
+//    describe("none", () => {
+//        it("returns true if 0 items in the array match", () => {
+//            const fn = (val) => val >= 10;
+//
+//            expect(lib.none([1,2,3], fn)).to.deep.equal(true)
+//            expect(lib.none([10,20,30], fn)).to.deep.equal(false)
+//            expect(lib.none([5, 10, 15], fn)).to.deep.equal(false)
+//            expect(lib.none([], fn)).to.deep.equal(true)
+//        })
+//
+//        it("does not use loops", checkForLoops('none'))
+//    })
+//
+//    describe("map", () => {
+//        it("returns an array containing elements transformed by the function", () => {
+//            const fn = (x) => x + 1;
+//
+//            expect(lib.map([1], fn)).to.deep.equal([2])
+//            expect(lib.map([1,2,3], fn)).to.deep.equal([2,3,4])
+//            expect(lib.map([20, 10 ,30], fn)).to.deep.equal([21,11,31])
+//            expect(lib.map([5, 10, 15, 2, -1, 4], fn)).to.deep.equal([6,11,16,3,0,5])
+//            expect(lib.map([], fn)).to.deep.equal([])
+//        })
+//
+//        it("does not use loops", checkForLoops('map'))
+//    })
+//
+//    describe("join", () => {
+//        it("returns a string with the elements of the array joined by the delimiter", () => {
+//            expect(lib.join([], ",")).to.eq("")
+//            expect(lib.join(["a"], ",")).to.eq("a")
+//            expect(lib.join(["a", "b"], ",")).to.eq("a,b")
+//            expect(lib.join(["a", "b", "c"], "|")).to.eq("a|b|c")
+//            expect(lib.join([1,2,3], "|")).to.eq("1|2|3")
+//        })
+//
+//        it("does not use loops", checkForLoops('join'))
+//    })
+//
+//    describe("split", () => {
+//        it("returns an array of strings split on the delimiter", () => {
+//            expect(lib.split("", '.')).to.deep.equal([])
+//            expect(lib.split("a", '.')).to.deep.equal(['a'])
+//            expect(lib.split("a.b", '.')).to.deep.equal(['a', 'b'])
+//        })
+//
+//        it("does not use loops", checkForLoops('split'))
+//    })
+//
+//    describe("reduce", () => {
+//        it("returns the reduced value", () => {
+//            const fn = (previous, current) => previous + current;
+//
+//            expect(lib.reduce([], fn, 5)).to.deep.equal(5)
+//            expect(lib.reduce([1], fn, 5)).to.deep.equal(6)
+//            expect(lib.reduce([1,2], fn, 5)).to.deep.equal(8)
+//            expect(lib.reduce([1,2,4], fn, 5)).to.deep.equal(12)
+//            expect(lib.reduce([1,2,4,10], fn, 5)).to.deep.equal(22)
+//        })
+//
+//        it("does not use loops", checkForLoops('reduce'))
+//    })
+//
+//    describe("indexOf", () => {
+//        it("returns the index of the given value", () => {
+//            expect(lib.indexOf([1,2,3], 1)).to.equal(0)
+//            expect(lib.indexOf([1,2,3], 2)).to.equal(1)
+//            expect(lib.indexOf([1,2,3], 3)).to.equal(2)
+//            expect(lib.indexOf([1,2,3], 4)).to.equal(-1)
+//        })
+//
+//        it("does not use loops", checkForLoops('indexOf'))
+//    })
+//
+//    describe("leftPad", () => {
+//        it("returns a string padded by the given delimiter, the given number of times", () => {
+//            expect(lib.leftPad('',      5, '-')).to.eq("-----")
+//            expect(lib.leftPad('a',     4, '|')).to.eq("|||a")
+//            expect(lib.leftPad('ab',    4, '|')).to.eq("||ab")
+//            expect(lib.leftPad('abc',   4, '|')).to.eq("|abc")
+//            expect(lib.leftPad('abcd',  4, '|')).to.eq("abcd")
+//            expect(lib.leftPad('abcde', 4, '|')).to.eq("abcde")
+//        })
+//
+//        it("does not use loops", checkForLoops('leftPad'))
+//    })
+//
+//    describe("flatten", () => {
+//        it("returns a single-dimensional array of all of the values", () => {
+//            expect(lib.flatten([])).to.deep.equal([])
+//            expect(lib.flatten([1])).to.deep.equal([1])
+//            expect(lib.flatten([[1]])).to.deep.equal([1])
+//            expect(lib.flatten([[[1]]])).to.deep.equal([1])
+//            expect(lib.flatten([[[[1]]]])).to.deep.equal([1])
+//            expect(lib.flatten([1, [2], [3,4], [ 5, [6, [7]]]])).to.deep.equal([1, 2, 3, 4, 5, 6, 7])
+//        })
+//
+//        it("does not use loops", checkForLoops('flatten'))
+//    })
+//})

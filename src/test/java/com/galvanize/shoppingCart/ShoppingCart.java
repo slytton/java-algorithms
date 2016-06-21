62eae4000628469c8b718c5d1b74027cd379c5ed
Seth Lytton <slytton@gmail.com>
2016-06-21 14:59:11 -0600

    Add initial shopping cart class and test files

---
+++ src/test/java/com/galvanize/shoppingCart/ShoppingCart.java
@@ -0,0 +1,7 @@
+package com.galvanize.shoppingCart;
+
+/**
+ * Created by gschool on 6/21/16.
+ */
+public class ShoppingCart {
+}
---
+++ src/test/java/com/galvanize/shoppingCart/ShoppingCartTest.groovy
@@ -0,0 +1,10 @@
+package com.galvanize.shoppingCart
+
+import spock.lang.Specification
+
+/**
+ * Created by gschool on 6/21/16.
+ */
+class ShoppingCartTest extends Specification {
+
+}
